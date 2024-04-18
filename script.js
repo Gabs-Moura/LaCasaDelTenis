884164f0c9acb2167270f5da465dedf879ea5c67
Gabs-Moura <gabrielmourasouza@hotmail.com>
2024-04-18 19:35:26 -0300

    Adicionando o JS

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function showModal(){
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function hideModal(){
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+}
+
